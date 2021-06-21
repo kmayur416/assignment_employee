1a4761fadd1cf784fe193560126e63e642f805f6
mayur <kmayur416@gmail.com>
2021-06-21 10:57:21 +0530

    First commit

---
+++ restfulapicrud/employeeapi/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    fullname = models.CharField(max_length=100)
+    emp_code=models.CharField(max_length=3)
+    mobile = models.CharField(max_length=15)
+
+#create or insert or add  new record--post
+
+
+# retreive inserted data-- get
+
+# update data  / edit data -- put
+
+# Delete /  remove -- Delete
